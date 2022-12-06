74d74bef621678447f4118d95db8780b7b776be3
Aswin Adi Saputra <aswin.adisaputra@gmail.com>
2022-12-06 15:46:28 +0700

    ht_login exercise

--- lib/config.dart
+++ lib/config.dart
@@ -1,7 +1,7 @@
 class AppConfig {
   //# CONFIG
-  static String yourName = "DenyOcr";
-  static String phoneNumber = "082146727433";
+  static String yourName = "Aswin";
+  static String phoneNumber = "081234187888";
   //# ---------------------------------------
   //# ---------------------------------------
 
